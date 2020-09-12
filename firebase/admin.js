67a2da7d37aeac95cef429314adb5f9b4932701d
Erik Martinez Roqueñi <me@erikmartinez.mx>
2020-09-11 19:36:19 -0500

    :bug: fix firebase acount json parser

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,12 +1,11 @@
 var admin = require('firebase-admin')
 
-// var serviceAccount = require('./firebase-keys.json')
 const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
-// console.log(process.env.FIREBASE_DATABASE_URL)
+
 try {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_DATABASE_URL, // 'https://celeste-app-mx.firebaseio.com',
+    credential: admin.credential.cert(JSON.parse(serviceAccount)),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
   })
 } catch (err) {
   if (!/already exists/.test(err.message)) {
